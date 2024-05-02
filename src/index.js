5a466c903d4805c4f6891fe58917cf2b49e28541
Nicholas Lachhman <nickla@my.yorku.ca>
2024-05-02 14:31:12 -0400

    Completed task 1: Firebase project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1Bf8MJ7Tsld3nyOLXefdOquKJ0h3eobo",
+  authDomain: "radical-firebase.firebaseapp.com",
+  projectId: "radical-firebase",
+  storageBucket: "radical-firebase.appspot.com",
+  messagingSenderId: "980130967825",
+  appId: "1:980130967825:web:2cf2ff6ea3dcee5eed1cb0",
+};
+
+initializeApp(firebaseConfig);
