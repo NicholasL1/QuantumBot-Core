f5d7a556b6e6a2fdb339469f14559a856581abf7
Nicholas Lachhman <nickla@my.yorku.ca>
2024-05-12 17:45:16 -0400

    Reverted UI

--- frontend/src/functions/constants.tsx
+++
@@ -1,7 +0,0 @@
-export const projectId = process.env.NEXT_PUBLIC_CHAT_ENGINE_PROJECT_ID
-  ? process.env.NEXT_PUBLIC_CHAT_ENGINE_PROJECT_ID
-  : "";
-
-export const privateKey: string = process.env.NEXT_PUBLIC_CHAT_ENGINE_PROJECT_KEY
-  ? process.env.NEXT_PUBLIC_CHAT_ENGINE_PROJECT_KEY
-  : "";
