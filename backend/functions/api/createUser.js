e1bae948788cf68829ee975a13ba3b06bc4f5b4d
Nicholas Lachhman <nickla@my.yorku.ca>
2024-05-16 18:37:02 -0400

    Changed createuser endpoint

--- backend/functions/api/createUser.js
+++ backend/functions/api/createUser.js
@@ -1,16 +1,20 @@
 const functions = require("firebase-functions");
 const axios = require("axios");
 
-// Create user endpoint for when a user is registered with google sign in
-exports.createUser = functions.auth.user().onCreate((user) => {
-  axios.post(
-    "https://api.chatengine.io/users/",
-    {
-      username: user.displayName,
-      secret: user.uid,
-      email: user.email,
-      avatar: user.photoURL,
-    },
-    { headers: { "Private-Key": "e672f363-5d46-4a96-8ed6-6d80c1d04af7" } }
-  );
+// Create user endpoint - adds user in chat engine
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+  try {
+    await axios.post(
+      "https://api.chatengine.io/users/",
+      {
+        username: user.displayName,
+        secret: user.uid,
+        email: user.email,
+      },
+      { headers: { "Private-Key": "e672f363-5d46-4a96-8ed6-6d80c1d04af7" } }
+    );
+    functions.logger.info("Successfully created user in Chat Engine.");
+  } catch (error) {
+    functions.logger.error("Error creating user in Chat Engine:", error);
+  }
 });
